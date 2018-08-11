8cc11d8e2e69638e6810388081cddecc065f3528
Joshua Watson <josh.m.watson@gmail.com>
2018-08-11 11:49:13 -0500

    world collisions

--- Assets/Scripts/Utils/FillCollider.cs
+++ Assets/Scripts/Utils/FillCollider.cs
@@ -6,11 +6,24 @@ public class FillCollider : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        var pc2 = gameObject.GetComponent<BoxCollider2D>();
+
+        int pointCount = 4;
+        MeshFilter mf = GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        Vector2[] points = new Vector2[pointCount];
+        points[0] = new Vector2(0, 0);
+        points[1] = new Vector2(0, pc2.size.x);
+        points[2] = new Vector2(0, pc2.size.y);
+        points[3] = new Vector2(pc2.size.x, pc2.size.y);
+
+        Vector3[] vertices = new Vector3[pointCount];
+        Vector2[] uv = new Vector2[pointCount];
+        for (int i = 0; i < pointCount; i++)
+        {
+            Vector2 actual = points[i];
+            vertices[i] = new Vector3(actual.x, actual.y, 0);
+        }
+
+    }
 }
