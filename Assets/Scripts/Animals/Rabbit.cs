838c49e4d7572fec140b77b2826d1f9984ab8587
Greg <halfskye@gmail.com>
2018-08-11 12:58:46 -0700

    Add Animal variants folder and basic Rabbit example

---
+++ Assets/Scripts/Animals/Rabbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbit : Animal {
+
+    //@TODO: Figure out how to reproduce rabbits and add them to HoldingPin
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
