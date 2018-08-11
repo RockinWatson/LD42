3d03aa848a286d31ed43c804a895199dc2c1463c
Greg <halfskye@gmail.com>
2018-08-11 12:59:32 -0700

    Add HoldingPin and HoldingPinMgr

---
+++ Assets/Scripts/HoldingPin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingPin : MonoBehaviour {
+
+    [SerializeField]
+    private float _pooCapacity = 10.0f;
+
+    private List<Animal> _animals;
+    public List<Animal> GetAnimals() {
+        return _animals;
+    }
+
+    public void SetAnimal(Animal.ANIMAL_TYPE type) {
+        //@TODO: Generate the animal prefabs.
+
+        //@TODO: Assign them to our animals list.
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/HoldingPinMgr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldingPinMgr : MonoBehaviour {
+
+    [SerializeField]
+	private HoldingPin[] _pins = null;
+	public HoldingPin GetPinByIndex(uint index) {
+		return _pins[index];
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
