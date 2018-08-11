a462a8c126a47564725e51c20ad072f6f593197f
Greg <halfskye@gmail.com>
2018-08-11 13:09:52 -0700

    Add basic PlayerInventory

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour {
+
+    private List<Poo> _poos = null;
+
+	private float _totalWeight = 0.0f;
+	float GetTotalWeight()
+	{
+		return _totalWeight;
+	}
+
+    private void Awake()
+    {
+        _poos = new List<Poo>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		_totalWeight = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void AddPoo(Poo poo) {
+        _poos.Add(poo);
+    }
+}
