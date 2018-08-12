81444e424546cb20f47cc27ef24292b18e4d7ee9
Greg <halfskye@gmail.com>
2018-08-11 22:59:22 -0700

    PlayerInventory: Pick up Poo behavior

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -3,8 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour {
+    
+    [SerializeField]
+    private float _pickUpRadius = 3.0f;
+
+    [SerializeField]
+    private float _weightCapacity = 100.0f;
+
+    [SerializeField]
+    private float _vacuumTime = 0.75f;
+
+    [SerializeField]
+    private float _vacuumSpeed = 0.05f;
 
     private List<Poo> _poos = null;
+    //private Dictionary<Poo, float> _vacuumList = null;
+
+    private List<Poo> _vacuumList = null;
+    //private List<VacuumItem> _vacuumList null;
 
 	private float _totalWeight = 0.0f;
 	float GetTotalWeight()
@@ -15,6 +31,8 @@ public class PlayerInventory : MonoBehaviour {
     private void Awake()
     {
         _poos = new List<Poo>();
+        //_vacuumList = new Dictionary<Poo, float>();
+        _vacuumList = new List<Poo>();
     }
 
     // Use this for initialization
@@ -24,9 +42,68 @@ public class PlayerInventory : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+        if(Input.GetKeyDown(KeyCode.E)) {
+            Debug.Log("PICK UP POO!!");
+            VacuumUpPoo();
+        }
+        UpdateVacuumList();
 	}
 
-    public void AddPoo(Poo poo) {
-        _poos.Add(poo);
+    private void VacuumUpPoo() {
+        Collider2D[] colliders;
+        colliders = Physics2D.OverlapCircleAll(transform.position, _pickUpRadius);
+        if(colliders.Length > 0) {
+            Debug.Log("FOUND SOME SHIT!");
+        }
+        for (int i = 0; i <= colliders.Length- 1; ++i) {
+            Poo poo = colliders[i].gameObject.GetComponent<Poo>();
+            if(poo) {
+                Debug.Log("GETTIN POO HITS!!");
+                float pooWeight = poo.GetWeight();
+                if(_weightCapacity - _totalWeight - pooWeight >= 0) {
+                    AddPoo(poo);
+                }
+            }
+        }
+    }
+
+    private void AddPoo(Poo poo) {
+        if (!_vacuumList.Contains(poo))
+        {
+            _vacuumList.Add(poo);
+        }
+        //_vacuumList.Add(new VacuumItem(poo, _vacuumTime));
+    }
+
+    class VacuumItem {
+        public Poo poo;
+        public float vacuumTime;
+        public VacuumItem(Poo shit, float time) {
+            poo = shit;
+            vacuumTime = time;
+        }
+    }
+
+    private void UpdateVacuumList() {
+        List<Poo> removeList = new List<Poo>();
+        for (int i = 0; i < _vacuumList.Count - 1; ++i) {
+            Poo poo = _vacuumList[i];
+            Vector3 pos = poo.transform.position;
+            poo.transform.position = Vector3.MoveTowards(pos, poo.transform.position, _vacuumSpeed);
+            //poo.transform.position = Vector3.Lerp(pos, this.transform.position, 0.5f);
+            if(Mathf.Epsilon >= (poo.transform.position - pos).sqrMagnitude) {
+                _poos.Add(poo);
+                removeList.Add(poo);
+            }
+        }
+        foreach (Poo poo in removeList)
+        {
+            _vacuumList.Remove(poo);
+            if (poo.Pin)
+            {
+                poo.Pin.RemovePoo(poo);
+            }
+            poo.gameObject.SetActive(false);
+        }
     }
 }
