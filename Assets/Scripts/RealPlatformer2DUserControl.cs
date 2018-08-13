e2dcefe2da2089a57de4f068501c4ed0a201669f
Joshua Watson <josh.m.watson@gmail.com>
2018-08-13 16:49:58 -0500

    Deleted CrossPlatform For Build Stuff

--- Assets/Scripts/RealPlatformer2DUserControl.cs
+++ Assets/Scripts/RealPlatformer2DUserControl.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
 
 namespace Assets.Scripts
 {
@@ -21,7 +20,7 @@ namespace Assets.Scripts
             if (!m_Jump)
             {
                 // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                m_Jump = Input.GetKeyDown(KeyCode.Space);
             }
         }
 
@@ -30,7 +29,7 @@ namespace Assets.Scripts
         {
             // Read the inputs.
             bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float h = Input.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
             m_Jump = false;
