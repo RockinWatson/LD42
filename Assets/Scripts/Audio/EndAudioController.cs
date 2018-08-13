62b8d33f9267e4d88ddd6406ad55b5e26688411c
Stephen Roper <stephenroper@me.com>
2018-08-13 17:09:05 -0500

    Added code for end audio functionality.

---
+++ Assets/Scripts/Audio/EndAudioController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAudioController : MonoBehaviour
+{
+
+    public AudioSource music;
+    public AudioSource select;
+    public AudioSource fart;
+
+    bool _selectCheck = false;
+
+    // Use this for initialization
+    void Awake()
+    {
+        InitAudio();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void InitAudio()
+    {
+        AudioSource[] audio = GetComponents<AudioSource>();
+        music = audio[0];
+        select = audio[1];
+        fart = audio[2];
+
+        fart.loop = false;
+        select.loop = false;
+        music.Play();
+        music.loop = false;
+
+        StartCoroutine(WaitForFart());
+
+    }
+
+    IEnumerator WaitForFart()
+    {
+        yield return new WaitForSeconds(7f);
+        fart.Play();
+    }
+}
